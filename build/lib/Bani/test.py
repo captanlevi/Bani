bb7ee52b01aad90c27d352e0d66c3a5858ebc593
DemonicMushy <47481731+DemonicMushy@users.noreply.github.com>
2021-06-11 02:00:07 +0800

    0.7.1: Added T5 generator as one of the generation methods

--- build/lib/Bani/test.py
+++ build/lib/Bani/test.py
@@ -1,43 +1,36 @@
 from common.utils import *
 from common.FAQ import *
-from sentence_transformers import  SentenceTransformer
-
+from sentence_transformers import SentenceTransformer
 from common.generation import IdentityProducer
-
-
 from common.generation import GeneratonManager
+
 q2L = load_dict("babybonus/question_to_label.pkl")
 a2L = load_dict("babybonus/answer_to_label.pkl")
 
-
-
 l2A = dict()
-for a,l in a2L.items():
+for a, l in a2L.items():
     l2A[l] = a
-
 
 questions = []
 answers = []
 
 limit = 1000
-for q,l in q2L.items():
+for q, l in q2L.items():
     questions.append(q)
     answers.append(l2A[l])
-    if(limit < 0):
+    if limit < 0:
         break
     limit -= 1
 
+Q, A = processRaw(questions, answers)
 
-Q,A = processRaw(questions,answers)
+faq = FAQ(name="babyBonus", questions=Q, answers=A)
+model = SentenceTransformer("roberta-base-nli-stsb-mean-tokens")
+# faq._paraphrase(Identitygenerator())
 
-
-faq = FAQ(name = "babyBonus", questions= Q, answers= A)
-model  = SentenceTransformer("roberta-base-nli-stsb-mean-tokens")
-#faq._paraphrase(Identitygenerator())
-
-faq.buildFAQ(model ,GeneratonManager([IdentityProducer]))
+faq.buildFAQ(model, GeneratonManager([IdentityProducer]))
 print(len(faq.FAQ))
-#faq.save('./')
+# faq.save('./')
 print(faq.isUsable())
 
 
--- setup.py
+++ setup.py
@@ -6,11 +6,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "Bani",
-    version = "0.6.3",
+    version = "0.7.1",
     author = "Rushi Babaria",
     url = "https://github.com/captanlevi/FAQ-QnA-matching.git",
     packages = setuptools.find_packages(),
-    install_requires = ["sentence_transformers==0.3.8", "nltk==3.5", "pandas", "numpy","regex","nlpaug","spacy", "tqdm"],
+    install_requires = ["sentence_transformers==0.3.8","nltk==3.5", "abbreviations==0.2.5", "pandas", "numpy","regex","nlpaug","spacy", "tqdm"],
     author_email="rjlinkin50@gmail.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
